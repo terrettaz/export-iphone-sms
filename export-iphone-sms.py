22137884aa1ce9b6bbb901cb916673897377ff83
Pierrick <pierrick@gmail.com>
2010-03-28 23:02:27 +0200

    refactoring. make SMSExporter more reusable

--- export-iphone-sms.py
+++ export-iphone-sms.py
@@ -13,18 +13,19 @@ desc = """SMS Exporter from iPhone backup
     ~/Library/Application Support/MobileSync
   * Read file and export the file in a readable format.
     format supported : 
-      - txt : Text plain format
-      - csv : Exchangable format as comma separated
-      - xml : Exchangable format as XML
+      - txt  : Text plain format
+      - csv  : Exchangable format as comma separated
+      - xml  : Exchangable format as XML
       - json : JavaScript Object Notation
 
   Usage: export-iphone-sms [options] format
-      format: txt, csv
+      format: txt, csv, xml json
       options
         -h              : print this help
         -E <encoding>   : encoding (default utf-8)
-        -q              : quiet    (default false)
         -b <backup_dir> : backup_dir (default see upper)
+        -f <out file>   : output file, if not set it export in stdout
+        -q              : quiet    (default false)
         -v              : print version
         -c              : check last version
   
@@ -45,12 +46,11 @@ desc = """SMS Exporter from iPhone backup
 import os, sys
 import getopt
 import sqlite3 as db # abstraction of database
-import csv
 from datetime import datetime
 
 # global variables
 quiet = False
-encoding = 'utf8'
+default_encoding = 'utf8'
 backup_dir = '%s/Library/Application Support/MobileSync' % os.path.expanduser('~')
 
 EXIT_SUCCESS = 0
@@ -60,6 +60,11 @@ EXIT_ERROR_EXPORTER = 3
 EXIT_ERROR_NOT_FOUND = 4
 EXIT_ERROR_VERSION_CHECK = 5
 
+##
+## This class export an iphone sms sqlite databse into
+## the stdout in various formats according the registered
+## exporters.
+##
 class SMSExporter(object):
     
     def __init__(self, sqlite_db, encoding='utf8'):
@@ -91,7 +96,6 @@ class SMSExporter(object):
         for row in c:
             self.data.append(self.__fetch_message(row))
         c.close()
-        log('database preloaded')
     
     def __loop_messages(self, callback):
         count = 0
@@ -110,16 +114,16 @@ class SMSExporter(object):
     def register(self, exporter):
         self.exporters[exporter.name] = exporter
     
-    def export(self, type):
+    def export(self, type, output):
         if type not in self.exporters:
-            log('Error: no exporter for "%s"' % type)
-            sys.exit(EXIT_ERROR_EXPORTER)
-            
+            raise Exception('Error: no exporter for "%s"' % type)
+        
+        if not isinstance(output, file): output = open(output, 'w') 
         exporter = self.exporters[type]
-        exporter.init(sys.stdout, self.encoding)
+        exporter.init(output, self.encoding)
         count = self.__loop_messages(exporter.export)
         exporter.end()
-        log('export finished, %(count)d sms' % locals())
+        return count
     
     def close(self):
         self.conn.close()
@@ -140,6 +144,109 @@ class SMSExporter(object):
         c.close()
         conn.close()
         return ret
+
+## -- Exporters -- ##
+class ExporterBase(object):
+    def init(self, output, encoding):
+        self.output = output
+        self.encoding = encoding
+
+    def export(self, message):
+        pass
+
+    def end(self):
+        pass
+
+    def _encode(self, uni):
+        return uni.encode(self.encoding)
+
+class TextExporter(ExporterBase):
+    name = 'txt'
+
+    def export(self, message):
+        if message['flags'] == 2:
+            pre = 'form'
+        else:
+            pre = 'to'
+        date = datetime.fromtimestamp(message['date'])
+        self.output.write(self._encode(u'\n message %d %s ' % (message['rowid'], pre)))
+        self.output.write(self._encode(message['address']))
+        self.output.write(self._encode(u' \n %s\n   ' % date))
+        self.output.write(self._encode(message['text']))
+        self.output.write(self._encode(u'\n---------\n'))
+
+class CSVExporter(ExporterBase):
+    name = 'csv'
+
+    def init(self, output, encoding):
+        import csv, cStringIO, codecs
+        super(CSVExporter, self).init(output, encoding)
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, delimiter=';', quoting=csv.QUOTE_ALL)
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def export(self, message):
+        fields = []
+        for field in message.values():
+            if isinstance(field, unicode):
+                field = field.encode('utf-8')
+            fields.append(field)
+        self.writer.writerow(fields)
+        data = self.queue.getvalue()
+        data = data.decode('utf-8')
+        data = self.encoder.encode(data)
+        self.output.write(data)
+        self.queue.truncate(0)
+
+
+class BufferedExporterBase(ExporterBase):
+    def init(self, output, encoding):
+        super(BufferedExporterBase, self).init(output, encoding)
+        self.messages = []
+
+    def export(self, message):
+        self.messages.append(message)
+
+class JSONExporter(BufferedExporterBase):
+    name = 'json'
+
+    def end(self):
+        try:
+            import json
+        except:
+            import simplejson as json
+
+        ret = json.dumps(self.messages, ensure_ascii = False)
+        self.output.write(self._encode(ret))
+
+class XMLMessageFactory(object):
+
+    def create(self, doc, message):
+        el_message = doc.createElement('message')
+        el_message.setAttribute('id', str(message['rowid']))
+        el_message.setAttribute('address', message['address'])
+        el_message.setAttribute('date', str(message['date']))
+        if message['flags'] == 2:
+            action = 'income'
+        else:
+            action = 'outcome'
+        el_message.setAttribute('action', action)
+        el_message.appendChild(doc.createTextNode(message['text']))
+        return el_message
+
+class XMLExporter(BufferedExporterBase):
+    name = 'xml'
+    def __init__(self, message_factory):
+        self.message_factory = message_factory
+
+    def end(self):
+        from xml.dom import getDOMImplementation
+        impl = getDOMImplementation()
+        doc = impl.createDocument(None, "messages", None)
+        el_messages = doc.documentElement
+        for message in self.messages:
+            el_messages.appendChild(self.message_factory.create(doc, message))
+        self.output.write(doc.toprettyxml(encoding = self.encoding))
 
 def usage(status=EXIT_ERROR):
     global desc
@@ -177,14 +284,18 @@ def get_last_version():
 
 def parse_argv(argv):
     try:
-        opts, args = getopt.getopt(argv[1:], 'E:b:qhvc')
+        opts, args = getopt.getopt(argv[1:], 'E:b:f:qhvc')
     except getopt.GetoptError:
         usage()
         
-    global quiet, encoding, backup_dir, version
+    global quiet, default_encoding, backup_dir, version
+    output = sys.stdout
+    encoding = default_encoding
     for o, v in opts:
         if o == '-E':
             encoding = v
+        if o == '-f':
+            output = v
         if o == '-q':
             quiet = True
         if o == '-v':
@@ -203,7 +314,7 @@ def parse_argv(argv):
         usage()
 
     format = args[0]
-    return (format, encoding, backup_dir, quiet)
+    return (format, encoding, backup_dir, quiet, output)
 
 def get_menu_choice(text, choices, quiet):
     if quiet:
@@ -232,113 +343,9 @@ def get_menu_choice(text, choices, quiet):
             log(e)
             log('not a number',4)
             break
-
-## Exporters
-class ExporterBase(object):
-    def init(self, needle, encoding):
-        self.needle = needle
-        self.encoding = encoding
-    
-    def export(self, message):
-        pass
-
-    def end(self):
-        pass
-    
-    def _encode(self, uni):
-        return uni.encode(self.encoding)
-
-class TextExporter(ExporterBase):
-    name = 'txt'
-    
-    def export(self, message):
-        if message['flags'] == 2:
-            pre = 'form'
-        else:
-            pre = 'to'
-        date = datetime.fromtimestamp(message['date'])
-        self.needle.write(self._encode(u'\n message %d %s ' % (message['rowid'], pre)))
-        self.needle.write(self._encode(message['address']))
-        self.needle.write(self._encode(u' \n %s\n   ' % date))
-        self.needle.write(self._encode(message['text']))
-        self.needle.write(self._encode(u'\n---------\n'))
-
-class CSVExporter(ExporterBase):
-    name = 'csv'
-
-    def init(self, needle, encoding):
-        import cStringIO
-        import codecs
-        super(CSVExporter, self).init(needle, encoding)
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, delimiter=';', quoting=csv.QUOTE_ALL)
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def export(self, message):
-        fields = []
-        for field in message.values():
-            if isinstance(field, unicode):
-                field = field.encode('utf-8')
-            fields.append(field)
-        self.writer.writerow(fields)
-        data = self.queue.getvalue()
-        data = data.decode('utf-8')
-        data = self.encoder.encode(data)
-        self.needle.write(data)
-        self.queue.truncate(0)
-        
-
-class BufferedExporterBase(ExporterBase):
-    def init(self, needle, encoding):
-        super(BufferedExporterBase, self).init(needle, encoding)
-        self.messages = []
-    
-    def export(self, message):
-        self.messages.append(message)
-    
-class JSONExporter(BufferedExporterBase):
-    name = 'json'
-    
-    def end(self):
-        try:
-            import json
-        except:
-            import simplejson as json
-            
-        ret = json.dumps(self.messages, ensure_ascii = False)
-        self.needle.write(self._encode(ret))
-
-class XMLMessageFactory(object):
-    
-    def create(self, doc, message, encoding):
-        el_message = doc.createElement('message')
-        el_message.setAttribute('id', str(message['rowid']))
-        el_message.setAttribute('address', message['address'])
-        el_message.setAttribute('date', str(message['date']))
-        if message['flags'] == 2:
-            action = 'income'
-        else:
-            action = 'outcome'
-        el_message.setAttribute('action', action)
-        el_message.appendChild(doc.createTextNode(message['text']))
-        return el_message
-
-class XMLExporter(BufferedExporterBase):
-    name = 'xml'
-    def __init__(self, message_factory):
-        self.message_factory = message_factory
-        
-    def end(self):
-        from xml.dom import getDOMImplementation
-        impl = getDOMImplementation()
-        doc = impl.createDocument(None, "messages", None)
-        el_messages = doc.documentElement
-        for message in self.messages:
-            el_messages.appendChild(self.message_factory.create(doc, message, self.encoding))
-        self.needle.write(doc.toprettyxml(encoding = self.encoding))
         
 def main(argv):
-    format, encoding, backup_dir, quiet = parse_argv(argv)
+    format, encoding, backup_dir, quiet, output = parse_argv(argv)
     
     path = []
     log('locating sms database..')
@@ -371,7 +378,12 @@ def main(argv):
     exporter.register(CSVExporter())
     exporter.register(XMLExporter(XMLMessageFactory()))
     exporter.register(JSONExporter())
-    exporter.export(type=format)
+    try:
+        count = exporter.export(format, output)
+        log('export finished, %(count)d sms' % locals())
+    except Exception, e:
+        log(e)
+        sys.exit(EXIT_ERROR_EXPORTER)
     exporter.close()
     
     return EXIT_SUCCESS
