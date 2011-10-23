f937526d41291b8407f985510295258a3bed4cde
Pierrick <pierrick@gmail.com>
2011-10-23 17:16:32 +0200

    fix with ios 5

--- export-iphone-sms.py
+++ export-iphone-sms.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 
-version = '0.7'
+version = '0.8'
 
 __version__ = "$Revision: %s $" % version
 __author__ = "Pierrick Terrettaz"
@@ -151,6 +151,8 @@ class SMSExporter(object):
             else:
                 ret = False
         except db.OperationalError:
+            ret = False
+        except db.DatabaseError:
             ret = False
         c.close()
         conn.close()
